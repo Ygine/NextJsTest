cd90eedb89c6d93ff16a7638ec034a003be49ade
vitvitskyi <vitvitskyi.vd@gmail.com>
2021-07-12 21:14:53 +0300

    add common js module

--- global.d.ts
+++ global.d.ts
@@ -1,11 +1,3 @@
-
-// declare module '*.svg' {
-//   import React = require('react');
-//   export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
-//   const content: any;
-//   export default content;
-// }
-
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpe';
